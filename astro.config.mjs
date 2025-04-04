ecb3e319de0c2f3739ede845fc2024f458271297
jhatting <jarrodhatting@icloud.com>
2025-04-04 11:59:04 -0500

    astro-icon included

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [icon()]
+});
